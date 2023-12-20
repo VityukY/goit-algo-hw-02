66ff10b8405280181d49db32709922e8e6dfe0d7
Vityuk Yuriy <you.thunderstruck@gmail.com>
2023-12-20 16:12:35 +0200

    add KeyboardInterrupt

--- practice_Queue.py
+++ practice_Queue.py
@@ -1,24 +1,30 @@
 from queue import Queue
 import time
 import uuid
+
 queue = Queue()
 
+
 def generate_request():
-    request = {'id':uuid.uuid4()}
+    request = {"id": uuid.uuid4()}
     queue.put(request)
-    print (f"Request with id: {request['id']} is succesfuly addet to Queue")
- 
+    print(f"Request with id: {request['id']} is successfully added to Queue")
 
 
 def process_request():
     if not queue.empty():
         request = queue.get()
-        print (f"Request with id: {request['id']} is processed")
+        print(f"Request with id: {request['id']} is processed")
     else:
-        print ('Queue is empty')
+        print("Queue is empty")
 
-while True:
-    generate_request()
-    time.sleep(1) #додав зупинку 1 сек, для повільнішого потоку
-    process_request()    
-    time.sleep (2) #додав зупинку 2 сек, для повільнішого потоку
+
+try:
+    while True:
+        generate_request()
+        time.sleep(1)  # додав зупинку 1 сек, для повільнішого потоку
+        process_request()
+        time.sleep(2)  # додав зупинку 2 сек, для повільнішого потоку
+
+except KeyboardInterrupt:
+    print("Exiting the program.")
